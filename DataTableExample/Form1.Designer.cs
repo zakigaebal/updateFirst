bb69f3aa1c2eb059ca0ed1c6e01eaddaef7d8a9e
zakigaebal <86054791+zakigaebal@users.noreply.github.com>
2022-01-17 18:55:58 +0900

    초기 커밋.

---
+++ DataTableExample/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace DataTableExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(459, 245);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(316, 276);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(141, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "업데이트";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 22);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "제거하기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 319);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DataTableExample/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DataTableExample
+{
+    public partial class Form1 : Form
+    {
+        DataTable dataTable = null;
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+          
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataTable = new DataTable();
+            dataTable.Columns.Add("NO", typeof(int));
+            dataTable.Columns.Add("이름", typeof(string));
+            dataTable.Columns.Add("입사 날짜", typeof(DateTime));
+            dataTable.Columns.Add("월급", typeof(int));
+
+            dataTable.Rows.Add(100, "김철수", "2003-06-17", 24000);
+            dataTable.Rows.Add(101, "김나나", "2005-09-21", 17000);
+
+            dataGridView1.DataSource = dataTable;
+
+        }
+      
+
+
+    
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DataRow row = dataTable.NewRow();
+
+            row["NO"] = 104;
+            row["이름"] = "홍길동";
+            row["입사 날짜"] = "2007-05-21";
+            row["월급"] = 6000;
+
+            dataTable.Rows.InsertAt(row, 0);
+
+            dataGridView1.DataSource = dataTable;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataTable.Rows[0].Delete();
+            }
+            catch
+            {
+            }
+        }
+    }
+}
